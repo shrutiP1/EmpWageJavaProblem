7183aa9dd863a34b792f591c8286a68a90ca4b65
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-07 03:33:11 +0530

    ADD: Calculate Employee Wage For full time Part Time

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,7 @@ package com.bridgelabz.emwage;
 public class EmpWage
 {
 	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HR= 20;
 	public static void checkUC1()
 	 {
@@ -39,6 +40,32 @@ public class EmpWage
 		 System.out.println("Emp Wage :"+empWage);
 		 
 	 }
+	 public static int calOption()
+	 {
+		 int res=(int)Math.floor(Math.random()*10)%3;
+		 return res;
+	 }
+	 public static void calWageUC3()
+	 {
+		 System.out.println("UC3 Output:");
+		 int empHr=0;
+		 int res=calOption();
+		 if(res==IS_FULL_TIME)
+		 {
+			 empHr=8;
+		 }
+		 else if(res==IS_PART_TIME)
+		 {
+			 empHr=4;
+		 }
+		 else
+		 {
+			 empHr=0;
+		 }
+		 int empWage=calSalary(empHr);
+		 System.out.println("Emp Wage :"+empWage);
+		 
+	 }
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Problem");
@@ -46,6 +73,8 @@ public class EmpWage
 		EmpWage.checkUC1();
 		//UC2
 		EmpWage.calWageUC2();
+		//UC3
+	    EmpWage.calWageUC3();
 	}
 
 }
